71efac62a6fcc7e2dd713c88d1c6cb3873752c5a
Ethan Smyth <ethan.smyth@hotmail.com>
2026-01-26 08:15:44 +0000

    chore: remove unused files

--- include/consumer.hpp
+++
@@ -1,9 +0,0 @@
-template <typename DataType>
-class Consumer
-{
-    public:
-    DataType Consume();
-
-    private:
-    std::size_t position;
-};
--- include/producer.hpp
+++
@@ -1,9 +0,0 @@
-template <typename DataType>
-class Producer
-{
-    public:
-    DataType Produce();
-
-    private:
-    std::size_t position;
-};
